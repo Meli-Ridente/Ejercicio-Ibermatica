37549d69886ea67da20bdad7eff970111531d8f6
Mel Ridente <melissaridente3e@gmail.com>
2025-01-09 04:29:03 -1100

    changes-counter

--- contador/src/app/page.tsx
+++ contador/src/app/page.tsx
@@ -27,13 +27,14 @@ export default function Home() {
   }
   return (
     <div className={styles.page}>
-      
       <main className={styles.main}>
+        <h2 style={{color:'green'}}>Counter roules📋: </h2>
+        <p>- You cannot add more than 15</p>
+        <p>- You cannot subtract less than 0</p>
       <Stack spacing={2} direction="row" alignItems={'center'}>
         <Button variant="text" size={'large'} onClick={() => handleCount('subs')}>-</Button>
         <h1 style={{fontSize:'100px'}}>{count}</h1>
         <Button variant="text" size={'large'} onClick={() => handleCount()}>+</Button>
-        
       </Stack>
       <Button variant="text" size={'large'} onClick={() => { setCount(0); setError('')}}>RESET</Button>
       {error && <p style={{color:'#FF4336'}}>{error}</p>}
