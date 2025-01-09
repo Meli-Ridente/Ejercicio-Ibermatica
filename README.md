cc43b7fb3e1b23b7946514d905d10ffd86041e64
Mel Ridente <melissaridente3e@gmail.com>
2025-01-09 03:57:36 -1100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Ejercicio-Ibermatica
+# Contador🚀
+
+¡Bienvenido a mi prueba!
+
+### Pre-requisitos 📋
+
+Luego de clonar el repositorio, tener instalado node y npm.
+
+### Instalación 🔧
+
+Si no tienes instalado node y npm sigue las instrucciones de la página de node: https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+
+Para instalar las dependencias del proyecto ejecuta el siguiente comando: 'npm install'
+
+Para levantar el proyecto necesitas escribir en la terminal 'npm run dev'.
+
+## Cómo usar el proyecto⚙️
+
+El contador no muestra el mensaje de máximo o mínimo cuando es un número diferente al establecido como limite.
+
+No permite restar a un número menor a 0.
+
+No permite sumar a un número mayor a 15.
+
+## Construido con 🛠️
+
+Curso básico de next.js (https://nextjs.org/learn/react-foundations/server-and-client-components)
+
+Documentanción de next.js (https://nextjs.org/docs/app/getting-started/layouts-and-pages)
+
+Botones (https://mui.com/material-ui/react-button/)
---
+++ contador/src/app/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+import styles from "./page.module.css";
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0)
+  const [error, setError] = useState('')
+  const handleCount = (operation = 'add') =>{
+    setError('')
+    if (operation === 'add'){
+      if (count === 15){
+        setError('Max count reatched')
+        return;
+      }
+      setCount(count + 1)
+      return;
+    } if (operation === 'subs'){
+      if(count === 0){
+        setError('Min count reatched')
+        return;
+      }
+      setCount(count - 1)
+      return;
+    }
+  }
+  return (
+    <div className={styles.page}>
+      
+      <main className={styles.main}>
+      <Stack spacing={2} direction="row" alignItems={'center'}>
+        <Button variant="text" size={'large'} onClick={() => handleCount('subs')}>-</Button>
+        <h1 style={{fontSize:'100px'}}>{count}</h1>
+        <Button variant="text" size={'large'} onClick={() => handleCount()}>+</Button>
+        
+      </Stack>
+      <Button variant="text" size={'large'} onClick={() => { setCount(0); setError('')}}>RESET</Button>
+      {error && <p style={{color:'#FF4336'}}>{error}</p>}
+      </main>
+    </div>
+  );
+}
